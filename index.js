a9d5f255940163fa981d70c6d5c28f5edadae49b
abdulsalamhamandoush <abdulsalamhamandoush22@gmail.com>
2023-06-18 04:54:15 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+const batteries = [
+  5,
+  8,
+  3,
+  1,
+  7,
+  2,
+  5
+];
+
+const totalBatteries = batteries.reduce(reducer, 0);
